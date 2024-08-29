ecbfdfae5ac83cd36bb6257a3e2b149fafd88057
shreyashkumar350 <shreyashkumar350@gmail.com>
2024-08-29 23:49:39 +0530

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+    define("DB_HOST", "localhost");  // The hostname of your MySQL server (localhost for local servers)
+    define("DB_USER", "root");       // The MySQL username (default is 'root' for XAMPP)
+    define("DB_PASS", "");           // The MySQL password (default is empty for XAMPP)
+    define("DB_NAME", "attendance_system");  // The name of the database you created
+?>
---
+++ inc/footer.php
@@ -0,0 +1,18 @@
+	<div class="container mt-3">
+		<div class="card card-body bg-light text-center">
+			<h3>
+				<!-- <span> &copy;</span> -->
+				<script type="text/javascript">
+					var d = new Date();
+					// document.write(d.getFullYear());
+				</script>
+				<span>
+					<a target="_blank" href="">
+						Project by Shreyash 1BY21IS158
+					</a>
+				</span>
+			</h3>
+		</div>
+	</div>
+</body>
+</html>
